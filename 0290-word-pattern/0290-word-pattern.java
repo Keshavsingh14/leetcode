fac3ec8f899b0694fa718f62fec91d93c4e551ed
Keshav Shakti Singh <106806940+spartan1412@users.noreply.github.com>
2023-01-01 10:26:56 +0530

    Time: 1 ms (95.36%), Space: 40.2 MB (84.96%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] arr= s.split(" ");
+        HashMap<Character, String> map = new HashMap<Character, String>();
+        if(arr.length!= pattern.length())
+            return false;
+        for(int i=0; i<arr.length; i++){
+            char c = pattern.charAt(i);
+            if(map.containsKey(c)){
+                if(!map.get(c).equals(arr[i]))
+                    return false;
+            }else{
+                if(map.containsValue(arr[i]))
+                    return false;
+                map.put(c, arr[i]);
+            }    
+        }
+        return true;
+    }
+}
